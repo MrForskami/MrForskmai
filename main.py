1e708de4153ef6b9f5b3ff6d438cc16f180467be
SashkaSashka <>
2021-11-30 18:29:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+s = "                                   '''"
+d = "                                  '    '"
+e = "                                 :______:"
+g = "                                 |      |"
+y = "                                 | саша |"
+g = "                                 |      |"
+w = "                                /|      |\                         ."
+v = "                               /_|______|_\                        ."
+n = "                                 /,,\ /,,\                         ."
+print(s)
+print(d)
+print(e)
+print(g)
+print(g)
+print(y)
+print(g)
+print(w)
+print(v)
+print(n)
